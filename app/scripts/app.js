e48b9a683dd5cb51fad006735b87e4b3fde2c2ee
Dean McCoy <Dean.McCoy@pressganey.com>
2016-07-14 15:10:23 -0500

    Form Changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,29 @@
 (function () {
 
     angular.module('minmax', ['jcs-autoValidate'])
-        .controller('MinMaxCtrl', function ($scope) {
+        .controller('MinMaxCtrl', function ($scope, $http) {
             $scope.formModel = {};
             $scope.onSubmit = function () {
-                
-                    console.log("Hey, it's submitted");
-                    console.log($scope.formModel);
+                console.log("Hey, it's submitted");
+                console.log($scope.formModel);
 
-                console.log("Invalid Form!")
+                $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+                    success(function (data) {
+                    console.log("This hit the server")
 
+                }).error(function (data) {
+                    console.log("blah blah");
+                });
             }
+
+        })
+
+        .run(function (defaultErrorMessageResolver) {
+        defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+            errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
+            errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
+            errorMessages['badUsername'] = 'Username can only contain numbers and letters';
+        });
     });
 
 })();
